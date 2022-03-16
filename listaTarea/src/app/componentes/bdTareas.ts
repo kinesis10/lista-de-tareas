01cad1b1389295ea91b4f76181fd0c99dffeba68
Chesini <pchesin1@gmail.com>
2022-03-16 20:11:59 -0300

    creacion de base de datos usando json

---
+++ listaTarea/src/app/componentes/bdTareas.ts
@@ -0,0 +1,27 @@
+import { Tareas } from "./tareas"
+export const TAREAS: Tareas[] = [
+    {
+        id: 1,
+        texto:  "Terminar el modulo de angular",
+        dia: "19 de Marzo",
+        recordar: true,
+
+    },
+    {
+        id: 2,
+        texto:  "terminar java",
+        dia: "15 de Abril",
+        recordar: true,
+
+    },
+    {
+        id: 3,
+        texto:  "Terminar el modulo de DeVops",
+        dia: "30 de Abril",
+        recordar: true,
+
+    }
+ 
+] 
+    
+
---
+++ listaTarea/src/app/componentes/tareas.ts
@@ -0,0 +1,7 @@
+export interface Tareas{
+    id: number,
+    texto: string,
+    dia: string,
+    recordar:boolean,
+
+}
